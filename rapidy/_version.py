1afbe44b929b066acad63741ca2521acd69d685c
Daniil Grois <daniil.grois@gmail.com>
2024-04-19 11:31:39 +0300

    features/mypy-support: Added mypy support.

--- rapidy/_version.py
+++ rapidy/_version.py
@@ -1,7 +1,9 @@
+from typing import Tuple
+
 from aiohttp import __version__
 
 
-def parse_version(version: str) -> tuple[int, ...]:
+def parse_version(version: str) -> Tuple[int, ...]:
     return tuple(map(int, version.partition('+')[0].split('.')))  # noqa: WPS221
 
 
