bed15560e185c4dc8f35a5a0f5d91813e401bb15
Daniil Grois <danii.popkov@x5.ru>
2024-05-07 21:29:07 +0300

    bug/fix-unannotated-handlers: Fixed a bug where unannotated standard aiohttp handlers could not be used.

--- rapidy/_annotation_container.py
+++ rapidy/_annotation_container.py
@@ -34,11 +34,15 @@ class AnnotationContainerAddFieldError(TypeError):
 
 
 class RequestFieldAlreadyExistError(Exception):
-    _base_err_msg = 'Error during attribute definition in the handler - request param defined twice.'
+    _base_err_msg = (
+        'Error during attribute definition in the handler - request param defined twice.'
+        'The error may be because the first attribute of the handler is not annotated.'
+        'By default, `rAPIdy` will pass `web.Request` to the first attribute if it has no type annotation.'
+    )
 
     def __init__(self, *args: Any, handler: Any):
         super().__init__(
-            f'{self._base_err_msg} {_create_handler_info_msg(handler)}',
+            f'\n\n{self._base_err_msg} {_create_handler_info_msg(handler)}',
             *args,
         )
 
@@ -342,15 +346,20 @@ def create_annotation_container(
     container = AnnotationContainer(handler=handler, handler_type=handler_type)
 
     endpoint_signature = inspect.signature(handler)
-    signature_params = endpoint_signature.parameters
+    signature_params = endpoint_signature.parameters.items()
 
-    for param_name, param in signature_params.items():
+    num_of_extracted_signatures = 0
+
+    for param_name, param in signature_params:
+        num_of_extracted_signatures += 1
+
         try:
             annotation, param_field_info, default = extract_handler_attr_annotations(param=param, handler=handler)
         except NotParameterError:
             if handler_type.is_func:
                 if not get_args(param.annotation):
-                    if issubclass(Request, param.annotation):
+                    # FIXME: Fix the processing logic for the 1-st attribute to return a specific error
+                    if issubclass(Request, param.annotation) or num_of_extracted_signatures == 1:
                         container.set_request_field(param_name)
 
             continue
@@ -366,6 +375,7 @@ def create_annotation_container(
                 )
             except AnnotationContainerAddFieldError as annotation_container_add_field_error:
                 raise RequestParamError(handler=handler) from annotation_container_add_field_error
+
         else:  # pragma: no cover
             raise
 
