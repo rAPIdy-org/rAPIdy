1f08d1147bc4893352dad888ff5b78d456e1ba7f
Daniil Grois <daniil.grois@gmail.com>
2024-05-09 20:11:55 +0300

    bug/fix-catch-middlewares: web-exceptions and web-responses refactor.

---
+++ tests/response/test_server_info.py
@@ -0,0 +1,19 @@
+import pytest
+from pytest_aiohttp.plugin import AiohttpClient
+
+from rapidy import web
+
+
+@pytest.mark.parametrize('server_info_in_response', [True, False])
+async def test_server_info(aiohttp_client: AiohttpClient, server_info_in_response: bool) -> None:
+    app = web.Application(server_info_in_response=server_info_in_response)
+
+    async def handler() -> web.Response:
+        return web.Response()
+
+    app.add_routes([web.post('/', handler)])
+    client = await aiohttp_client(app)
+
+    resp = await client.post('/')
+    assert resp.status == 200
+    assert bool(resp.headers.get('Server')) == server_info_in_response
